29480a018efc81945d9975430effd0d03e47d2be
rahul2870 <rahulkashyap2870@gmail.com>
2023-08-04 04:16:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -10,5 +10,13 @@ export default function App() {
             <Route path="/" element={<Frontend />} />
             <Route path="/all-forms" element={<Backend />} />
         </Routes>
+
+        <div className="text-center py-10">
+            <h1 className="text-lg">Hi, Quick Connect</h1>
+            <div className="flex gap-10 justify-center mt-4 font-bold">
+                <p>+91-8448443891</p>
+                <p>+91-9310964293</p>
+            </div>
+        </div>
     </>
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -35,11 +35,11 @@ export default function NavbarCMP() {
 
                     <div class="relative ml-3">
                         <div>
-                            <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                            <a href='https://github.com/rahul2870' type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
                                 <img class="h-8 w-8 rounded-full" src="https://avatars.githubusercontent.com/u/59166413?v=4" alt="" />
-                            </button>
+                            </a>
                         </div>
                     </div>
                 </div>
